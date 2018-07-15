86bee619d3d398db3c52ea0e413c58de4fc3e8b8
Marco Antônio <ma.souza.junior@gmail.com>
2018-07-15 18:23:21 -0300

    ENH : Styling code

--- python_sandbox/__init__.py
+++ python_sandbox/__init__.py
@@ -1,5 +1,8 @@
 #! env python3
+"""This script just print Hello world."""
+import sys
 from greetings import greeting
 
 if __name__ == "__main__":
     greeting()
+    print(sys.argv)
--- python_sandbox/greetings.py
+++ python_sandbox/greetings.py
@@ -1,2 +1,6 @@
+"""This is just a utils script."""
+
+
 def greeting():
+    """greeting function."""
     print("Hello world")
